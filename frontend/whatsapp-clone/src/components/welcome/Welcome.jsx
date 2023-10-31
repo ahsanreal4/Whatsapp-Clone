b9fb7b6997146e97966e30ff33fc2790a315ef7b
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2023-10-31 13:33:42 +0500

    some fixes

--- frontend/whatsapp-clone/src/components/welcome/Welcome.jsx
+++ frontend/whatsapp-clone/src/components/welcome/Welcome.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Welcome.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Welcome.module.css";
 function Welcome() {
   return (
-    <>
+    <div className={styles.main}>
       <div className={styles.container}>
-      <h1 className={styles.welcome_title}>Welcome To My Chat App</h1>
-      <div className={styles.button_container}>
-        <Link to="/signup" className={styles.nav_link }>
-          <button className={styles.button_style }>Sign Up</button>
-        </Link>
-        <Link to="/signin" className={styles.nav_link }>
-          <button className={styles.button_style }>Sign In</button>
-        </Link>
-      </div>
+        <h1 className={styles.welcome_title}>Welcome To My Chat App</h1>
+        <div className={styles.button_container}>
+          <Link to="/signup" className={styles.nav_link}>
+            <button className={styles.button_style}>Sign Up</button>
+          </Link>
+          <Link to="/signin" className={styles.nav_link}>
+            <button className={styles.button_style}>Sign In</button>
+          </Link>
+        </div>
       </div>
       <footer>Made by Ahsan And Faizan</footer>
-    </>
+    </div>
   );
 }
 
