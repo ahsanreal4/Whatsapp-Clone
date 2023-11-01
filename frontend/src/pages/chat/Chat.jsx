481b05394c12862e5a826ccaff78a70f82946f33
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2023-11-01 20:27:33 +0500

    chat ui fix

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -7,27 +7,32 @@ function Chat() {
 
   return (
     <>
-    <div className={styles.main_container}>
-      {chats.map((chat, index) => (
-          <div key={index} className={styles.user_container}>
-           
-            <p>{chat.participants[0].name}</p>
-            <img src="https://footwearnews.com/wp-content/uploads/2022/12/Cha-Eun-Woo-2.jpg" alt="pic"  height={50} width={50}/>
+      <div className={styles.main_container}>
+        <div className={styles.users_container}>
+          {chats.map((chat, index) => (
+            <div key={`chat-box-${index}`} className={styles.user_container}>
+              <img
+                src="https://footwearnews.com/wp-content/uploads/2022/12/Cha-Eun-Woo-2.jpg"
+                alt="pic"
+                height={50}
+                width={50}
+              />
+              <p>{chat.participants[0].name}</p>
             </div>
-         
-        ))}
-      <div className={styles.sent_messages_container}>
-        <div className={styles.user_chat_container}>
-          <h3 className={styles.first_color}>ahsan</h3>
-          <h3 className={styles.second_user}>fazan</h3>
+          ))}
         </div>
-        <div className={styles.input_messages_container}>
-          <input type="text" />
-          <br />
-          <button>➢</button>
+        <div className={styles.sent_messages_container}>
+          <div className={styles.user_chat_container}>
+            <h3 className={styles.first_color}>ahsan</h3>
+            <h3 className={styles.second_user}>fazan</h3>
+          </div>
+          <div className={styles.input_messages_container}>
+            <input type="text" />
+            <br />
+            <button>➢</button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
