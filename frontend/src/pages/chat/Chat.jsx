9e985f3f42dbee96bedd43929dd87647ff02417b
Faizan707 <faizanbutt707@gmail.com>
2023-11-02 13:59:47 +0500

    changing in chat styling and add a condition in chat page

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -8,18 +8,28 @@ function Chat() {
   return (
     <>
       <div className={styles.main_container}>
-        <div className={styles.users_container}>
-          {chats.map((chat, index) => (
-            <div key={`chat-box-${index}`} className={styles.user_container}>
-              <img
-                src="https://footwearnews.com/wp-content/uploads/2022/12/Cha-Eun-Woo-2.jpg"
-                alt="pic"
-                height={50}
-                width={50}
-              />
-              <p>{chat.participants[0].name}</p>
-            </div>
-          ))}
+        <div className={styles.main_container}>
+          <div className={styles.users_container}>
+            <h1>Chats</h1>
+            {chats.length === 0 ? (
+              <p>No Chats</p>
+            ) : (
+              chats.map((chat, index) => (
+                <div
+                  key={`chat-box-${index}`}
+                  className={styles.user_container}
+                >
+                  <img
+                    src="https://footwearnews.com/wp-content/uploads/2022/12/Cha-Eun-Woo-2.jpg"
+                    alt="pic"
+                    height={50}
+                    width={50}
+                  />
+                  <p>{chat.participants[0].name}</p>
+                </div>
+              ))
+            )}
+          </div>
         </div>
         <button className={styles.logout_button} onClick={logout}>
           <i className="fa-solid fa-right-from-bracket"></i>
